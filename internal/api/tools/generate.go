a17b4bd028e82a532d10bb6d238532a554cd4a3e
Josie, Go! <237042894+josiego@users.noreply.github.com>
2026-02-05 22:52:32 -0500

    testing2

--- internal/api/tools/generate.go
+++
@@ -1,4 +0,0 @@
-package tools
-
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config server.cfg.yaml ../../../docs/openapi.yaml
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config types.cfg.yaml ../../../docs/openapi.yaml
